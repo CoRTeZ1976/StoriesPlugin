24caabbfa2e82edc5020f96fdc70fd65124aaddd
Alex <specmashalexeysh@gmail.com>
2022-09-27 11:24:33 +0300

    Add slide toggle

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
-document.querySelector('.player-chunk-prev').addEventListener('click', function () {
-	function moveClass(className, activeClassName) {
-		let activeChunk = document.querySelector('.' + activeClassName),
-		prev = activeChunk.previousElementSibling;
-		if (prev && prev.classList.contains(className)) {
-			activeChunk.classList.remove(activeClassName);
-			activeChunk.previousElementSibling.classList.add(activeClassName);
-		}
-	}
+function initPlayer() {
 	
-	moveClass('timeline-chunk','timeline-chunk-active');
-	moveClass('player-chunk','player-chunk-active');
+}
+
+document.querySelector('.player-chunk-prev').addEventListener('click', () => {
+	moveClass('player-chunk-active', 'previousElementSibling');
+	
+	const
+		el = moveClass('timeline-chunk-active', 'previousElementSibling');
+	
+	if (el) {
+		el.querySelector('.timeline-chunk-inner').style.width = '';
+	}
 });
 
-document.querySelector('.player-chunk-next').addEventListener('click', function () {
-	function moveClass(className, activeClassName) {
-		let activeChunk = document.querySelector('.' + activeClassName),
-			prev = activeChunk.nextElementSibling;
-		if (prev && prev.classList.contains(className)) {
-			activeChunk.classList.remove(activeClassName);
-			activeChunk.nextElementSibling.classList.add(activeClassName);
-		}
+document.querySelector('.player-chunk-next').addEventListener('click', next);
+
+function moveClass(className, method) {
+	
+	const activeChunk = document.querySelector('.' + className),
+		next = activeChunk[method];
+	
+	if (next) {
+		activeChunk.classList.remove(className);
+		next.classList.add(className);
+		
+		return activeChunk;
 	}
 	
-	moveClass('timeline-chunk','timeline-chunk-active');
-	moveClass('player-chunk','player-chunk-active');
-});
+	return null;
+}
+
+function next() {
+	moveClass('player-chunk-active', 'nextElementSibling');
+	
+	const
+		el = moveClass('timeline-chunk-active', 'nextElementSibling');
+	
+	if (el) {
+		el.querySelector('.timeline-chunk-inner').style.width = '';
+	}
+}
+
+function runInterval(time, step) {
+	setInterval(() => {
+		const
+			activeChunk = document.querySelector('.timeline-chunk-active').querySelector('.timeline-chunk-inner');
+		
+		let
+			w = parseFloat(activeChunk.style.width) || 0;
+		
+		if (w === 100) {
+			next();
+			return;
+		}
+		
+		activeChunk.style.width = String(w + step) + '%';
+		
+	}, time * 1000 * step / 100);
+}
+
+runInterval(5, 1);
